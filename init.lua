51402ac9539570522a3c70b8e0210d23258769c6
Thomas Lo <thomasjinlo@gmail.com>
2026-04-19 08:20:36 -0700

    add markdown renderer

--- init.lua
+++ init.lua
@@ -1046,6 +1046,12 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    opts = {},
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
